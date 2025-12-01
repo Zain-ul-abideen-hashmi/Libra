a29167f4caabd3d139f4e9b83e2e4773119bb1f1
Zain-ul-abideen-hashmi <09-131242-093@student.bahria.edu.pk>
2025-12-01 20:08:22 +0500

    Added a more professional way to close program explainantion in comments in main cpp and in the file bros read this

--- Libra main.cpp
+++ Libra main.cpp
@@ -8,5 +8,8 @@ int main() {
     int choice=runMenu();
     cout<<" you selected option "<<choice<<endl;
 
+
+    cin.ignore();
+    cin.get(); // added this so program only closes when we click enter instead of closing automatically
     return 0;
 }
